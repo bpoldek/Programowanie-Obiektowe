142c9ce349ac6a085fa82b1481467e0e15a1dff7
Bartłomiej Polkowski <bpoldek@gmail.com>
2018-03-07 13:23:56 +0100

    Pierwszy program

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+    {
+       
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string legitymacjaStudenta;
+            legitymacjaStudenta = student.dodajStudenta();
+            legitymacjaStudenta = student.policzavg();
+            Console.ReadKey();*/
+
+           
+           
+                klasa.Student NowyStudent = new klasa.Student();
+                NowyStudent.DodajStudenta();
+            
+        }
+        
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/klasa/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.klasa
+{
+    class Student
+    {
+
+        public Student()
+        {
+
+        }
+
+        int indeks ;
+        double ocena1;
+        double ocena2;
+        double ocena3;
+        public void DodajStudenta()
+        {
+ 
+            Console.WriteLine("Podaj numer indeksu");
+            indeks = Console.Read();
+            Console.WriteLine("Podaj ocene z przedmiotu pierwszego");
+            ocena1 = Console.Read();
+            Console.WriteLine("Podaj ocene z przedmiotu drugiego");
+            ocena2 = Console.Read();
+            Console.WriteLine("Podaj ocene z przedmiotu trzeciego");
+            ocena3 = Console.Read();
+            var sum = ocena1 + ocena2 + ocena3;
+            var avg = sum / 3;
+            Console.WriteLine("Twoja średnia z ocen to:" + avg);
+            Console.ReadLine();
+        }
+
+    }
+}
+
